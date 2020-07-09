165341831a8faf2a267a433b417bd33ff5d309c6
zeroko14 <503969402@qq.com>
2020-07-09 13:47:43 +0800

    初始化文件

---
+++ teamviewerClient/teamviewerClient.h
@@ -0,0 +1,32 @@
+
+// teamviewerClient.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CteamviewerClientApp: 
+// 有关此类的实现，请参阅 teamviewerClient.cpp
+//
+
+class CteamviewerClientApp : public CWinApp
+{
+public:
+	CteamviewerClientApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CteamviewerClientApp theApp;
---
+++ teamviewerClient/teamviewerClientDlg.h
@@ -0,0 +1,33 @@
+
+// teamviewerClientDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CteamviewerClientDlg 对话框
+class CteamviewerClientDlg : public CDialogEx
+{
+// 构造
+public:
+	CteamviewerClientDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TEAMVIEWERCLIENT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
